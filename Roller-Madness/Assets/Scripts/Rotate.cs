1b2b4e03b084de968edcdc4cad343580566f5f1f
Thomas Abela <sethabela91@gmail.com>
2016-05-29 02:32:25 +0200

    Documentation Update
    All Tasks completed with several screenshots added to the Images folder.
    
    Only one Distinction Task remains.

---
+++ Roller-Madness/Assets/Scripts/Rotate.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Rotate : MonoBehaviour {
+	
+	public float speed = 70.0f;//change made here
+	
+	public enum whichWayToRotate {AroundX, AroundY, AroundZ}
+
+    //variable renamed here
+	public whichWayToRotate direction = whichWayToRotate.AroundX;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        //variable renamed here
+		switch(direction)
+		{
+		case whichWayToRotate.AroundX:
+			transform.Rotate(Vector3.right * Time.deltaTime * speed*5);
+			break;
+		case whichWayToRotate.AroundY:
+			transform.Rotate(Vector3.up * Time.deltaTime * speed * 5);
+			break;
+		case whichWayToRotate.AroundZ:
+			transform.Rotate(Vector3.forward * Time.deltaTime * speed * 5);
+			break;
+		}	
+	}
+}
